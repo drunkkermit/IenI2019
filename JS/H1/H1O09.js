dfb86e3bb867bea9865ab9a27c2b91652630d5cf
drunk kermit <151392@csgaugustinus.nl>
2019-12-19 10:10:16 +0000

    bncvj

--- JS/H1/H1O09.js
+++ JS/H1/H1O09.js
@@ -4,7 +4,7 @@ var marge = 20;
 function setup() {
   canvas = createCanvas(1000,300);
   canvas.parent('processing');
-  textFont("Verdana");
+  textFont("Glass Beads");
   textSize(letterGrootte);
   textLeading(1.2*letterGrootte);
   //noLoop();
@@ -14,7 +14,7 @@ function draw() {
   background('orange');
   fill('yellow');
   textAlign(LEFT, TOP);
-  text("mouseX:" + mouseX + "\nmouseY:" + mouseY,mouseX,mouseY);
+  text("mouseX:" +round(mouseX) + "mouseY:" +round(mouseY),mouseX,mouseY);
   translate(50,50);
   fill('black');
   text("vensterGrootte:\n" + windowWidth + " x " + windowHeight,0,0);
@@ -29,6 +29,6 @@ function draw() {
   translate(25,25);
   noStroke();
   fill('white');
-  textAlign(CENTER, CENTER);
+  textAlign(LEFT, CENTER);
   text("Deze tekst is erg lang en wordt daarom op meerdere regels weergegeven.",0,0,350,150);
 }
--- JS/test.js
+++ JS/test.js
@@ -8,10 +8,5 @@ function setup() {
 function draw() {
   noStroke();
 
-  fill('black');
-  ellipse(225,225,300);
-
-  noStroke();
-  fill('pink');
-  rect(125,125,200,200);
+for 
 }
